cbc8bf4a215ecae7ccf6db3e35fa0db0a41af010
Stagiaires11 <Stagiaires11P@ad.fms-ea.com>
2022-08-18 16:04:35 +0200

    recuperation d'une mini vesrion

--- booking-front/src/app/admin/components/admin-hotels/admin-hotels.component.ts
+++ booking-front/src/app/admin/components/admin-hotels/admin-hotels.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hotel } from 'src/app/models/hotel';
+import { HotelService } from 'src/app/services/hotel.service';
 
 @Component({
   selector: 'app-admin-hotels',
@@ -8,10 +9,18 @@ import { Hotel } from 'src/app/models/hotel';
 })
 export class AdminHotelsComponent implements OnInit {
   listHotels: Hotel[] |null= null;
-  availableRooms: number =0;
-  constructor() { }
+  availableRooms!: any[] ;
+  constructor(private hotelService: HotelService) { }
 
   ngOnInit(): void {
+    this.hotelService.getAllHotels().subscribe(hotels=>{
+      this.listHotels=hotels;
+      let arr: any[]= []
+      for(let i=0; i<hotels.length; i++){
+        arr.push(JSON.parse(JSON.stringify((hotels[i])))['rooms'].length)
+      }
+      this.availableRooms=arr 
+    })
   }
 
   onDeleteHotel(hotel: Hotel){
